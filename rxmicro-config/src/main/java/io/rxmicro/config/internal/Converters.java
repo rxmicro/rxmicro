26bdd682e7c067850b405ec7271ba0a25741f94e
rxmicro <rxmicro.io@gmail.com>
2020-12-08 13:52:54 +0200

    Refactor Converters class

--- rxmicro-config/src/main/java/io/rxmicro/config/internal/Converters.java
+++ rxmicro-config/src/main/java/io/rxmicro/config/internal/Converters.java
@@ -16,11 +16,9 @@
 
 package io.rxmicro.config.internal;
 
-import io.rxmicro.common.util.Reflections;
 import io.rxmicro.config.ConfigException;
+import io.rxmicro.config.internal.component.ToCustomTypeConverter;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.Duration;
@@ -41,14 +39,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.function.Function;
 
 import static io.rxmicro.common.util.ExCollectors.toUnmodifiableOrderedMap;
-import static io.rxmicro.common.util.Strings.startsWith;
 import static io.rxmicro.config.Config.KEY_VALUE_ENTRY_DELIMITER;
 import static io.rxmicro.config.Config.VALUES_DELIMITER;
 import static java.util.Collections.unmodifiableSortedSet;
@@ -59,6 +55,8 @@ import static java.util.Map.entry;
  * @since 0.1
  */
 public final class Converters {
+
+    private static final ToCustomTypeConverter TO_CUSTOM_TYPE_CONVERTER = new ToCustomTypeConverter();
 
     private static final Map<Class<?>, Function<String, Object>> CONVERTERS_MAP;
 
@@ -104,12 +102,15 @@ public final class Converters {
         map.put(ZoneId.class, ZoneId::of);
         map.put(Period.class, Period::parse);
         // Collections
-        map.put(List.class, s -> List.of(s.split(String.valueOf(VALUES_DELIMITER))));
-        map.put(Set.class, s -> Set.of(s.split(String.valueOf(VALUES_DELIMITER))));
-        map.put(SortedSet.class, s -> unmodifiableSortedSet(new TreeSet<>(Arrays.asList(s.split(String.valueOf(VALUES_DELIMITER))))));
+        map.put(List.class, s -> List.of(getConvertedWithoutTypeDefinitionValues(s)));
+        map.put(Set.class, s -> Set.of(getConvertedWithoutTypeDefinitionValues(s)));
+        map.put(SortedSet.class, s -> unmodifiableSortedSet(new TreeSet<>(Arrays.asList(getConvertedWithoutTypeDefinitionValues(s)))));
         map.put(Map.class, s -> Arrays.stream(s.split(String.valueOf(VALUES_DELIMITER)))
                 .map(pair -> pair.split(String.valueOf(KEY_VALUE_ENTRY_DELIMITER)))
-                .map(d -> entry(d[0], d[1]))
+                .map(d -> entry(
+                        convertWithoutTypeDefinition(d[0], false, false),
+                        convertWithoutTypeDefinition(d[1], false, false)
+                ))
                 .collect(toUnmodifiableOrderedMap(Map.Entry::getKey, Map.Entry::getValue)));
         // Init CONVERTERS_MAP
         CONVERTERS_MAP = Map.copyOf(map);
@@ -127,95 +128,12 @@ public final class Converters {
             if (converter != null) {
                 return converter.apply(value);
             } else {
-                return convertToCustomType(type, value).orElseThrow(() -> {
+                return TO_CUSTOM_TYPE_CONVERTER.convertToCustomType(type, value).orElseThrow(() -> {
                     throw new ConfigException(
                             "Can't convert '?' to '?'", String.class.getName(), type.getName()
                     );
                 });
             }
-        }
-    }
-
-    private static Optional<Object> convertToCustomType(final Class<?> type,
-                                                        final String value) {
-        if (startsWith(value, '@')) {
-            final int delimiter = value.indexOf(':');
-            if (delimiter != -1) {
-                final String fullClassName = value.substring(1, delimiter);
-                try {
-                    final Class<?> fullClass = Class.forName(value.substring(1, delimiter));
-                    final String constName = value.substring(delimiter + 1);
-                    if (fullClass.isEnum()) {
-                        return useEnumConstant(type, fullClass, constName);
-                    } else {
-                        return usePublicStaticFinalConstant(type, fullClass, constName);
-                    }
-                } catch (final ClassNotFoundException ignore) {
-                    throw new ConfigException(
-                            "Can't convert '?' to '?', because '?' class not defined!",
-                            String.class.getName(), type.getName(), fullClassName
-                    );
-                }
-            }
-        }
-        return Optional.empty();
-    }
-
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    private static Optional<Object> useEnumConstant(final Class<?> type,
-                                                    final Class<?> fullClass,
-                                                    final String constName) {
-        try {
-            return Optional.of(Enum.valueOf((Class<? extends Enum>) fullClass, constName));
-        } catch (final IllegalArgumentException ignore) {
-            throw new ConfigException(
-                    "Can't convert '?' to '?', because '?' ? does not contain '?' enum constant!",
-                    String.class.getName(), type.getName(), fullClass.getName(), getKind(fullClass), constName
-            );
-        }
-    }
-
-    private static Optional<Object> usePublicStaticFinalConstant(final Class<?> type,
-                                                                 final Class<?> fullClass,
-                                                                 final String constName) {
-        try {
-            final Field declaredField = fullClass.getDeclaredField(constName);
-            if (!Modifier.isStatic(declaredField.getModifiers())) {
-                throw new ConfigException(
-                        "Can't convert '?' to '?', because '?' field declared at '?' ? not static!",
-                        String.class.getName(), type.getName(), constName, fullClass.getName(), getKind(fullClass)
-                );
-            }
-            if (!Modifier.isPublic(declaredField.getModifiers())) {
-                throw new ConfigException(
-                        "Can't convert '?' to '?', because '?' field declared at '?' ? not public!",
-                        String.class.getName(), type.getName(), constName, fullClass.getName(), getKind(fullClass)
-                );
-            }
-            if (!Modifier.isFinal(declaredField.getModifiers())) {
-                throw new ConfigException(
-                        "Can't convert '?' to '?', because '?' field declared at '?' ? not final!",
-                        String.class.getName(), type.getName(), constName, fullClass.getName(), getKind(fullClass)
-                );
-            }
-            return Optional.of(Reflections.getFieldValue((Object) null, declaredField));
-        } catch (final NoSuchFieldException ignore) {
-            throw new ConfigException(
-                    "Can't convert '?' to '?', because '?' ? does not contain '?' public static final field!",
-                    String.class.getName(), type.getName(), fullClass.getName(), getKind(fullClass), constName
-            );
-        }
-    }
-
-    private static String getKind(final Class<?> fullClass) {
-        if (fullClass.isEnum()) {
-            return "enum";
-        } else if (fullClass.isInterface()) {
-            return "interface";
-        } else if (fullClass.isAnnotation()) {
-            return "annotation";
-        } else {
-            return "class";
         }
     }
 
@@ -283,7 +201,16 @@ public final class Converters {
                 // return string value
             }
         }
-        return value;
+        return TO_CUSTOM_TYPE_CONVERTER.convertToCustomType("undefined", value).orElse(value);
+    }
+
+    private static Object[] getConvertedWithoutTypeDefinitionValues(final String source) {
+        final String[] sourceArray = source.split(String.valueOf(VALUES_DELIMITER));
+        final Object[] destinationArray = new Object[sourceArray.length];
+        for (int i = 0; i < sourceArray.length; i++) {
+            destinationArray[i] = convertWithoutTypeDefinition(sourceArray[i], false, false);
+        }
+        return destinationArray;
     }
 
     private Converters() {
---
+++ rxmicro-config/src/main/java/io/rxmicro/config/internal/component/ToCustomTypeConverter.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2020. https://rxmicro.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.rxmicro.config.internal.component;
+
+import io.rxmicro.common.util.Reflections;
+import io.rxmicro.config.ConfigException;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Optional;
+
+import static io.rxmicro.common.util.Strings.startsWith;
+
+/**
+ * @author nedis
+ * @since 0.7.2
+ */
+public final class ToCustomTypeConverter {
+
+    public Optional<Object> convertToCustomType(final Class<?> type,
+                                                final String value) {
+        return convertToCustomType(type.getName(), value);
+    }
+
+    public Optional<Object> convertToCustomType(final String destinationType,
+                                                final String value) {
+        if (startsWith(value, '@')) {
+            final int delimiter = value.indexOf(':');
+            if (delimiter != -1) {
+                final String fullClassName = value.substring(1, delimiter);
+                try {
+                    final Class<?> fullClass = Class.forName(value.substring(1, delimiter));
+                    final String constName = value.substring(delimiter + 1);
+                    if (fullClass.isEnum()) {
+                        return useEnumConstant(destinationType, fullClass, constName);
+                    } else {
+                        return usePublicStaticFinalConstant(destinationType, fullClass, constName);
+                    }
+                } catch (final ClassNotFoundException ignore) {
+                    throw new ConfigException(
+                            "Can't convert '?' to '?', because '?' class not defined!",
+                            String.class.getName(), destinationType, fullClassName
+                    );
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private Optional<Object> useEnumConstant(final String destinationType,
+                                             final Class<?> fullClass,
+                                             final String constName) {
+        try {
+            return Optional.of(Enum.valueOf((Class<? extends Enum>) fullClass, constName));
+        } catch (final IllegalArgumentException ignore) {
+            throw new ConfigException(
+                    "Can't convert '?' to '?', because '?' ? does not contain '?' enum constant!",
+                    String.class.getName(), destinationType, fullClass.getName(), getKind(fullClass), constName
+            );
+        }
+    }
+
+    private Optional<Object> usePublicStaticFinalConstant(final String destinationType,
+                                                          final Class<?> fullClass,
+                                                          final String constName) {
+        try {
+            final Field declaredField = fullClass.getDeclaredField(constName);
+            if (!Modifier.isStatic(declaredField.getModifiers())) {
+                throw new ConfigException(
+                        "Can't convert '?' to '?', because '?' field declared at '?' ? not static!",
+                        String.class.getName(), destinationType, constName, fullClass.getName(), getKind(fullClass)
+                );
+            }
+            if (!Modifier.isPublic(declaredField.getModifiers())) {
+                throw new ConfigException(
+                        "Can't convert '?' to '?', because '?' field declared at '?' ? not public!",
+                        String.class.getName(), destinationType, constName, fullClass.getName(), getKind(fullClass)
+                );
+            }
+            if (!Modifier.isFinal(declaredField.getModifiers())) {
+                throw new ConfigException(
+                        "Can't convert '?' to '?', because '?' field declared at '?' ? not final!",
+                        String.class.getName(), destinationType, constName, fullClass.getName(), getKind(fullClass)
+                );
+            }
+            return Optional.of(Reflections.getFieldValue((Object) null, declaredField));
+        } catch (final NoSuchFieldException ignore) {
+            throw new ConfigException(
+                    "Can't convert '?' to '?', because '?' ? does not contain '?' public static final field!",
+                    String.class.getName(), destinationType, fullClass.getName(), getKind(fullClass), constName
+            );
+        }
+    }
+
+    private String getKind(final Class<?> fullClass) {
+        if (fullClass.isEnum()) {
+            return "enum";
+        } else if (fullClass.isInterface()) {
+            return "interface";
+        } else if (fullClass.isAnnotation()) {
+            return "annotation";
+        } else {
+            return "class";
+        }
+    }
+}
