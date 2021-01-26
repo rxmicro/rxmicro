05ff63955d766dfb39664bf838093c9e8e1c3ec4
rxmicro <rxmicro.io@gmail.com>
2021-01-26 21:06:14 +0200

    Add @StartsWith constraint

---
+++ rxmicro-validation/src/main/java/io/rxmicro/validation/constraint/StartsWith.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2020. https://rxmicro.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.rxmicro.validation.constraint;
+
+import io.rxmicro.validation.base.ConstraintRule;
+import io.rxmicro.validation.internal.SelfDocumented;
+import io.rxmicro.validation.validator.StartsWithConstraintValidator;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+/**
+ * The annotated element value must start with the provided prefix.
+ *
+ * @author nedis
+ * @see StartsWithConstraintValidator
+ * @since 0.1
+ */
+@Documented
+@Retention(SOURCE)
+@Target({FIELD, METHOD, PARAMETER})
+@ConstraintRule(
+        supportedTypes = String.class,
+        validatorClass = StartsWithConstraintValidator.class
+)
+@SelfDocumented
+public @interface StartsWith {
+
+    /**
+     * Allows disabling the validation rule if this rule is inherited from super class.
+     *
+     * <p>
+     * By default, disable is off.
+     *
+     * @return  {@code true} if the validation must be disabled.
+     */
+    boolean off() default false;
+
+    /**
+     * Returns the required prefix for the field value.
+     *
+     * @return the required prefix for the field value.
+     */
+    String value();
+}
---
+++ rxmicro-validation/src/main/java/io/rxmicro/validation/validator/StartsWithConstraintValidator.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2020. https://rxmicro.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.rxmicro.validation.validator;
+
+import io.rxmicro.http.error.ValidationException;
+import io.rxmicro.rest.model.HttpModelType;
+import io.rxmicro.validation.ConstraintValidator;
+
+/**
+ * Validator for the {@link io.rxmicro.validation.constraint.StartsWith} constraint.
+ *
+ * @author nedis
+ * @see io.rxmicro.validation.constraint.StartsWith
+ * @since 0.9
+ */
+public final class StartsWithConstraintValidator implements ConstraintValidator<String> {
+
+    private final String prefix;
+
+    /**
+     * Creates the default instance of {@link StartsWithConstraintValidator} type.
+     *
+     * @param prefix the required prefix for the field value.
+     */
+    public StartsWithConstraintValidator(final String prefix) {
+        this.prefix = prefix;
+    }
+
+    @Override
+    public void validate(final String actual,
+                         final HttpModelType httpModelType,
+                         final String modelName) {
+        if (actual != null && !actual.startsWith(prefix)) {
+            throw new ValidationException(
+                    "Invalid ? \"?\": Expected that value starts with '?' prefix, but actual is '?'!",
+                    httpModelType, modelName, prefix, actual
+            );
+        }
+    }
+}
