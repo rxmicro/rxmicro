a3371719459d2170fafcbc2a2c53ccc4e6a62587
rxmicro <rxmicro.io@gmail.com>
2021-11-08 18:57:28 +0200

    Fix Requires issue

--- rxmicro-common/src/main/java/io/rxmicro/common/util/Requires.java
+++ rxmicro-common/src/main/java/io/rxmicro/common/util/Requires.java
@@ -17,7 +17,6 @@
 package io.rxmicro.common.util;
 
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.function.Supplier;
 
 import static io.rxmicro.common.util.Formats.format;
@@ -34,28 +33,31 @@ public final class Requires {
      * Checks that the specified object reference is not {@code null}.
      *
      * <p>
-     * The main advantage of this method comparing with {@link Objects#requireNonNull(Object)} is the ability to replace
+     * The main advantage of this method comparing with {@link java.util.Objects#requireNonNull(Object)} is the ability to replace
      * {@code if} statement by the {@code assert} one.
-     * This replacement can increase the performance of the RxMicro framework in future.
+     * This replacement can increase the performance of the RxMicro framework.
      *
      * @param instance the specified object
      * @param <T> the type of the reference
      * @return {@code obj} if not {@code null}
      * @throws NullPointerException if the specified object is {@code null}
-     * @see Objects#requireNonNull(Object)
+     * @see java.util.Objects#requireNonNull(Object)
      */
     public static <T> T require(final T instance) {
         //assert instance != null;
-        return Objects.requireNonNull(instance);
+        if (instance == null) {
+            throw new NullPointerException();
+        }
+        return instance;
     }
 
     /**
      * Checks that the specified object reference is not {@code null} and throws a exception with custom message if it is.
      *
      * <p>
-     * The main advantage of this method comparing with {@link Objects#requireNonNull(Object, String)} is the ability to replace
+     * The main advantage of this method comparing with {@link java.util.Objects#requireNonNull(Object, String)} is the ability to replace
      * {@code if} statement by the {@code assert} one.
-     * This replacement can increase the performance of the RxMicro framework in future.
+     * This replacement can increase the performance of the RxMicro framework.
      *
      * @param instance the specified object
      * @param message the message template
@@ -64,20 +66,23 @@ public final class Requires {
      * @return {@code obj} if not {@code null}
      * @throws NullPointerException if the message template is {@code null} or the specified object is {@code null}
      * @throws IllegalArgumentException if detected a redundant placeholder or missing argument
-     * @see Objects#requireNonNull(Object, String)
+     * @see java.util.Objects#requireNonNull(Object, String)
      */
     public static <T> T require(final T instance,
                                 final String message,
                                 final Object... args) {
         //assert instance != null : format(message, args);
-        return Objects.requireNonNull(instance, format(message, args));
+        if (instance == null) {
+            throw new NullPointerException(format(message, args));
+        }
+        return instance;
     }
 
     /**
      * Checks that the specified object reference is not {@code null} and throws a exception with custom message if it is.
      *
      * <p>
-     * The main advantage of this method comparing with {@link Objects#requireNonNull(Object, String)} is the ability to replace
+     * The main advantage of this method comparing with {@link java.util.Objects#requireNonNull(Object, String)} is the ability to replace
      * {@code if} statement by the {@code assert} one.
      * This replacement can increase the performance of the RxMicro framework.
      *
@@ -88,14 +93,17 @@ public final class Requires {
      * @return {@code obj} if not {@code null}
      * @throws NullPointerException if the message template is {@code null} or the specified object is {@code null}
      * @throws IllegalArgumentException if detected a redundant placeholder or missing argument
-     * @see Objects#requireNonNull(Object, String)
+     * @see java.util.Objects#requireNonNull(Object, String)
      */
     @SafeVarargs
     public static <T> T require(final T instance,
                                 final String message,
                                 final Supplier<Object>... suppliers) {
-        //assert instance != null : format(message, Arrays.stream(suppliers).map(Supplier::get).toArray());
-        return Objects.requireNonNull(instance, format(message, Arrays.stream(suppliers).map(Supplier::get).toArray()));
+        //assert instance != null : format(message, args);
+        if (instance == null) {
+            throw new NullPointerException(format(message, Arrays.stream(suppliers).map(Supplier::get).toArray()));
+        }
+        return instance;
     }
 
     private Requires() {
